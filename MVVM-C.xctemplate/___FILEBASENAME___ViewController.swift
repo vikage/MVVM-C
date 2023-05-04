38b1f548e0b960226c2e4d2682ff33fcea47e9bf
linhnd99 <linhnd1@solarapp.asia>
2023-05-04 19:19:08 +0700

    Fix memory leak in view controller

--- MVVM-C.xctemplate/___FILEBASENAME___ViewController.swift
+++ MVVM-C.xctemplate/___FILEBASENAME___ViewController.swift
@@ -4,7 +4,8 @@ import UIKit
 
 class ___VARIABLE_sceneName___ViewController: UIViewController {
     var viewModel: ___VARIABLE_sceneName___ViewModel
-    var coordinator: ___VARIABLE_sceneName___Coordinator?
+    weak var coordinator: ___VARIABLE_sceneName___Coordinator?
+
     init(viewModel: ___VARIABLE_sceneName___ViewModel, coordinator: ___VARIABLE_sceneName___Coordinator) {
         self.viewModel = viewModel
         self.coordinator = coordinator
@@ -14,6 +15,7 @@ class ___VARIABLE_sceneName___ViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
